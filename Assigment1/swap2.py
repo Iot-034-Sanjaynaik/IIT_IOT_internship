67607c568d72cc35f276e5e4c54f1b5c9e353eff
sanjau03426 <sanjaynaik03426@gmail.com>
2025-12-12 22:19:34 +0530

    Assigment1

---
+++ Assigment1/swap2.py
@@ -0,0 +1,12 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("\nBefore swapping:")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =",b)
